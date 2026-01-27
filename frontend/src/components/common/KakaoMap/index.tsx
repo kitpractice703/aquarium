f036fd8083490d1f15556d1954b34aadd01f6e57
intae kim <intaekim@intaeui-MacBookAir.local>
2026-01-28 03:23:23 +0900

    build

--- frontend/src/components/common/KakaoMap/index.tsx
+++ frontend/src/components/common/KakaoMap/index.tsx
@@ -5,19 +5,22 @@ const KakaoMap = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // window.kakao가 있는지 확인 (index.html에 스크립트가 있어야 함)
+    // @ts-ignore
     if (window.kakao && window.kakao.maps) {
+      // @ts-ignore
       window.kakao.maps.load(() => {
         if (!containerRef.current) return;
 
         const options = {
+          // @ts-ignore
           center: new window.kakao.maps.LatLng(37.4915, 126.724), // 부평역
           level: 3,
         };
-
+        // @ts-ignore
         const map = new window.kakao.maps.Map(containerRef.current, options);
-
+        // @ts-ignore
         const markerPosition = new window.kakao.maps.LatLng(37.4915, 126.724);
+        // @ts-ignore
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
         });
