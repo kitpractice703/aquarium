58fbbe8c58253d875bfd130638f34d1b6c8343d9
intaekim <kitpractice703@gmail.com>
2026-01-29 13:04:55 +0900

    fix: vercel deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <GlobalStyle />
     {/* [FIX] basename 속성 추가!
         이제 라우터가 '/aquarium' 뒤에 오는 주소부터 읽기 시작합니다. */}
-    <BrowserRouter basename="/aquarium">
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
